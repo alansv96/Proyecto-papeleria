82e762c4514b6fd02b549127ea56fad599323910
alansv96 <alansv96@gmail.com>
2022-11-29 17:10:51 -0600

    se agrega funcion busqueda a navBar y se elimiman elementos de footer

---
+++ pagina_nosotros/assets/js/nosotros.js
@@ -0,0 +1,16 @@
+
+
+const searchInputDropdown = document.getElementById('search');
+
+searchInputDropdown.addEventListener("submit",(evento)=>{
+
+    evento.preventDefault()
+    
+
+    const valor=document.getElementById("floatingInput").value;
+    console.log(valor);
+   
+    //console.log(evento.target);
+    searchInputDropdown.reset()
+
+})
